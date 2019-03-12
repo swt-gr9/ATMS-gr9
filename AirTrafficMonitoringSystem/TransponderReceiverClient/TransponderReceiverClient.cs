5e2eb3410786cbaa189c8e9863973ab990e6761e
Morten Rosenquist <201706031@post.au.dk>
2019-03-12 14:31:26 +0100

    Tilføjet event i client

--- AirTrafficMonitoringSystem/TransponderReceiverClient/TransponderReceiverClient.cs
+++ AirTrafficMonitoringSystem/TransponderReceiverClient/TransponderReceiverClient.cs
@@ -9,7 +9,8 @@ namespace AirTrafficMonitoringSystem.TransponderReceiverClient
     {
         private ITransponderReceiver receiver;
         private IDataFormatter _dataFormatter;
-        
+        public event InformationReceivedHandler InformationReceived;
+
         public TransponderReceiverClient(ITransponderReceiver receiver, IDataFormatter dataFormatter)
         {
             _dataFormatter = dataFormatter;
@@ -21,14 +22,14 @@ namespace AirTrafficMonitoringSystem.TransponderReceiverClient
 
         private void ReceiverOnTransponderDataReady(object sender, RawTransponderDataEventArgs e)
         {
-            
-
             List<Plane.Plane> tempPlanes = new List<Plane.Plane>();
+
             foreach (var data in e.TransponderData)
             {
                 tempPlanes.Add(_dataFormatter.FormatFromString(data));
             }
-            
+
+            if (InformationReceived != null) InformationReceived(this, tempPlanes);
         }
     }
 }

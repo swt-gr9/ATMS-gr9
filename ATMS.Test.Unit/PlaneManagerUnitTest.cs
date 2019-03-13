29b0c19fce11c7b7deef54445481b4fe9d731255
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-03-13 11:42:05 +0100

    Planemanager fortsat

--- ATMS.Test.Unit/PlaneManagerUnitTest.cs
+++ ATMS.Test.Unit/PlaneManagerUnitTest.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using AirTrafficMonitoringSystem.Plane;
 using AirTrafficMonitoringSystem.PlaneManager;
 using AirTrafficMonitoringSystem.TransponderReceiverClient;
+using Microsoft.SqlServer.Server;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -18,7 +19,8 @@ namespace ATMS.Test.Unit
     {
         private List<Plane> NewPlanes = new List<Plane>();
         private List<Plane> OldPlanes = new List<Plane>();
-
+        private PlaneManager uut;
+        private ITransponderReceiverClient tr;
 
         [Test]
         public void QuickPlaneTest()
@@ -41,37 +43,99 @@ namespace ATMS.Test.Unit
             Assert.That(p1 != p2, Is.True);
         }
 
-        private int Called = 0;
+        [SetUp]
+        public void Setup()
+        {
+            tr = Substitute.For<ITransponderReceiverClient>();
+            uut = new PlaneManager(tr);
+            uut.PlaneNotify += EventHandler;
+        }
+
+        public List<Plane> AddNewPlanes()
+        {
+            List<Plane> pl = new List<Plane>();
+            pl.Add(new Plane
+            {
+                Altitude = 2000,
+                ID = "FD200",
+                TimeStamp = Parse("20191205121105200"),
+                XPosition = 5000,
+                YPosition = 20000
+            });
+            pl.Add(new Plane
+            {
+                Altitude = 10000,
+                ID = "FL100",
+                TimeStamp = Parse("20191205121106300"),
+                XPosition = 37000,
+                YPosition = 10000
+            });
+
+            return pl;
+        }
+
         [Test]
         public void TestAddingNewPlanes()
         {
-           List<Plane> pl = new List<Plane>();
-           pl.Add(new Plane{Altitude = 2000,
-               ID = "FD200",
-               TimeStamp = Parse("20191205121105200"),
-               XPosition = 5000,
-               YPosition = 20000
-           });
-           pl.Add(new Plane
-           {
-               Altitude = 10000,
-               ID = "FL100",
-               TimeStamp = Parse("20191205121106300"),
-               XPosition = 37000,
-               YPosition = 10000
-           });
 
-           ITransponderReceiverClient fako = Substitute.For<ITransponderReceiverClient>();
-
-           
-           PlaneManager pm = new PlaneManager(fako);
-           pm.PlaneNotify += EventHandler;
-
-           fako.ItemArrivedReceived += Raise.Event<InformationReceivedHandler>
+           List<Plane> pl = AddNewPlanes();
+           tr.ItemArrivedReceived += Raise.Event<InformationReceivedHandler>
                (this, new PlaneDetectedEvent { planes = pl });
 
            
            Assert.That(NewPlanes, Is.EqualTo(pl));
+        }
+
+        [Test]
+        public void TestCorrectID()
+        {
+            List<Plane> pl = AddNewPlanes();
+            tr.ItemArrivedReceived += Raise.Event<InformationReceivedHandler>
+                (this, new PlaneDetectedEvent {planes = pl});
+
+            Assert.That(NewPlanes[0], Is.EqualTo(pl[0]));
+        }
+
+        [Test]
+        public void NewPlanesNotUpdate()
+        {
+            List<Plane> pl = AddNewPlanes();
+            tr.ItemArrivedReceived += Raise.Event<InformationReceivedHandler>
+                (this, new PlaneDetectedEvent {planes = pl});
+
+            Assert.That(OldPlanes != pl, Is.True);
+        }
+
+        [Test]
+        public void TestUpdatePlanes()
+        {
+            List<Plane> pl = AddNewPlanes();
+            tr.ItemArrivedReceived += Raise.Event<InformationReceivedHandler>
+                (this, new PlaneDetectedEvent {planes = pl});
+
+            List<Plane> pl2 = new List<Plane>();
+            pl.Add(new Plane
+            {
+                Altitude = 2000,
+                ID = "FD200",
+                TimeStamp = Parse("20191205121106200"),
+                XPosition = 7000,
+                YPosition = 18000
+            });
+            pl.Add(new Plane
+            {
+                Altitude = 10000,
+                ID = "FL100",
+                TimeStamp = Parse("20191205121107300"),
+                XPosition = 32000,
+                YPosition = 11000
+            });
+
+
+            tr.ItemArrivedReceived += Raise.Event<InformationReceivedHandler>
+                (this, new PlaneDetectedEvent {planes = pl2});
+
+            Assert.That(OldPlanes[0], Is.EqualTo(pl[0]));
         }
 
         private void EventHandler(object sender, PlaneUpdateEvent e)

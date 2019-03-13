b7d3dffafcce8b366210ea4994ebfcd60ae2e2be
Morten Rosenquist <201706031@post.au.dk>
2019-03-13 09:01:49 +0100

    Tilføjet InformationReceveid test af eventet bliver created

--- ATMS.Test.Unit/TransponderReceiverClientTest.cs
+++ ATMS.Test.Unit/TransponderReceiverClientTest.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AirTrafficMonitoringSystem.Calculator;
 using AirTrafficMonitoringSystem.DataFormatter;
+using AirTrafficMonitoringSystem.Plane;
 using AirTrafficMonitoringSystem.TransponderReceiverClient;
 using NSubstitute;
 using NSubstitute.ReceivedExtensions;
@@ -21,18 +22,23 @@ namespace ATMS.Test.Unit
         private TransponderReceiverClient _uut;
         private IDataFormatter _dataFormatter;
         private ITransponderReceiver _transponderReceiver;
-        private int _transponderEventsRecieved;
+        private int _numberOfEvents;
 
 
         [SetUp]
         public void SetUp()
         {
+            _numberOfEvents = 0;
+
             _dataFormatter = Substitute.For<IDataFormatter>();
             _transponderReceiver = Substitute.For<ITransponderReceiver>();
 
             _uut = new TransponderReceiverClient(_transponderReceiver, _dataFormatter);
+
+            _uut.InformationReceived += (sender, args) => _numberOfEvents++;
         }
 
+        
 
         [TestCase("ATR423;39045;12932;14000;20151006213456789")]
         [TestCase("BCD123;10005;85890;12000;20151006213456789")]
@@ -48,8 +54,19 @@ namespace ATMS.Test.Unit
             _dataFormatter.Received(1).FormatFromString(input);
         }
 
-        
+        [TestCase("ATR423;39045;12932;14000;20151006213456789")]
+        [TestCase("BCD123;10005;85890;12000;20151006213456789")]
+        [TestCase("XYZ987;25059;75654;4000;20151006213456789")]
+        public void ReceiverOnTransponderDataReady_CreateEvent_InformationReceivedNumberOfEventsEqual1(string input)
+        {
+            
+            List<string> testData = new List<string>();
+            testData.Add(input);
 
+            _transponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
 
+            Assert.AreEqual(1,_numberOfEvents);
+        }
     }
 }

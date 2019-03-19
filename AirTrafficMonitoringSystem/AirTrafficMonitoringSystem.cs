8b79412e65b8c8504865a5652b34c2fa75239373
Morten Rosenquist <201706031@post.au.dk>
2019-03-19 17:20:18 +0100

    Tilføjet applikation

--- AirTrafficMonitoringSystem/AirTrafficMonitoringSystem.cs
+++ AirTrafficMonitoringSystem/AirTrafficMonitoringSystem.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficMonitoringSystem.DataFormatter;
+using AirTrafficMonitoringSystem.Display;
+using AirTrafficMonitoringSystem.PlaneManager;
+using AirTrafficMonitoringSystem.TransponderReceiverClient;
 using TransponderReceiver;
+
 
 namespace AirTrafficMonitoringSystem
 {
     public class AirTrafficMonitoringSystem
     {
-        private ITransponderReceiver ItransponderReceiver;
+        private TransponderReceiverFactory TransponderReceiverFactory;
+        private ITransponderReceiver TransponderReceiver;
+        private IDataFormatter DataFormatter;
+        private ITransponderReceiverClient TransponderReceiverClient;
+        private IPlaneManager PlaneManager;
+        private Display.Display Display;
+        private ILogger Logger;
+
+        public AirTrafficMonitoringSystem()
+        {
+            TransponderReceiverFactory = new TransponderReceiverFactory();
+            TransponderReceiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+            DataFormatter = new DataFormatter.DataFormatter();
+            TransponderReceiverClient = new TransponderReceiverClient.TransponderReceiverClient(TransponderReceiver,DataFormatter);
+            PlaneManager = new PlaneManager.PlaneManager(TransponderReceiverClient);
+            Logger = new ConsoleLogger();
+            Display = new Display.Display(PlaneManager,Logger);
+        }
     }
 }

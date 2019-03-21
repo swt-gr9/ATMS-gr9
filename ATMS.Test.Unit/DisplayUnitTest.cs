8c1041e4de881967a5306f2a099c064d46941d68
Chris <christopherbernold@icloud.com>
2019-03-21 23:25:05 +0100

    Removed commented out lines of code

--- ATMS.Test.Unit/DisplayUnitTest.cs
+++ ATMS.Test.Unit/DisplayUnitTest.cs
@@ -46,22 +46,6 @@ namespace ATMS.Test.Unit
             logger.Received(1).LogText($"{p1.ID} colliding with {p2.ID}");
         }
 
-        //[TestCase("FJ20")]
-        //public void TestNewPlaneDisplayID(string planeID)
-        //{
-        //    List<Plane> testList = new List<Plane>();
-
-        //    Plane p1 = new Plane { ID = planeID };
-
-        //    testList.Add(p1);
-
-        //    pm.PlaneNotify += Raise.Event<PlaneUpdate>(this, new PlaneUpdateEvent { NewPlanes = testList });
-
-        //    logger.Received(1).LogText("New Plane info:");
-        //    logger.Received(1).LogText($"Plane ID: {p1.ID}");
-
-        //}
-
         [TestCase("FJ20",14000, 39045, 12950)]
         public void TestNewPlaneDisplayID(string planeID, int alt, int xPos, int yPos)
         {
@@ -81,30 +65,6 @@ namespace ATMS.Test.Unit
 
         }
 
-        //[TestCase(14000)]
-        //public void TestNewPlaneDisplayAltitude(int alt)
-        //{
-        //    List<Plane> testList = new List<Plane>();
-
-        //    Plane p1 = new Plane{Altitude = alt};
-
-        //    testList.Add(p1);
-        //    pm.PlaneNotify += Raise.Event<PlaneUpdate>(this, new PlaneUpdateEvent { NewPlanes = testList });
-        //    logger.Received(1).LogText($"Plane Altitude: {p1.Altitude}");
-        //}
-
-        //[TestCase(39045, 12950)]
-        //public void TestNewPlaneDisplayPosition(int xPos, int yPos)
-        //{
-        //    List<Plane> testList = new List<Plane>();
-
-        //    Plane p1 = new Plane { XPosition = xPos, YPosition = yPos};
-
-        //    testList.Add(p1);
-        //    pm.PlaneNotify += Raise.Event<PlaneUpdate>(this, new PlaneUpdateEvent { NewPlanes = testList });
-        //    logger.Received(1).LogText($"Plane Postion, x: {p1.XPosition}, y: {p1.YPosition}");
-        //}
-
         [TestCase("FJ20", 13567, 40000, 13000, 100.276, 300.276)]
         public void TestUpdatedPlaneDisplayID(string planeID,int alt, int xPos, int yPos, double _heading, double _hSpeed)
         {
@@ -121,54 +81,5 @@ namespace ATMS.Test.Unit
             logger.Received(1).LogText("-------------------------------------------------------------------------------------------------------------------------------------------");
 
         }
-
-        //    [TestCase(13567)]
-        //    public void TestUpdatedPlaneDisplayAltitude(int alt)
-        //    {
-        //        List<Plane> testList = new List<Plane>();
-
-        //        Plane p1 = new Plane { Altitude = alt };
-
-        //        testList.Add(p1);
-        //        pm.PlaneNotify += Raise.Event<PlaneUpdate>(this, new PlaneUpdateEvent { UpdatedPlanes = testList});
-        //        logger.Received(1).LogText($"Plane Altitude: { p1.Altitude}");
-
-        //    }
-
-        //    [TestCase(40000, 13000)]
-        //    public void TestUpdatedPlaneDisplayPos(int xPos, int yPos)
-        //    {
-        //        List<Plane> testList = new List<Plane>();
-
-        //        Plane p1 = new Plane { XPosition = xPos, YPosition = yPos};
-
-        //        testList.Add(p1);
-        //        pm.PlaneNotify += Raise.Event<PlaneUpdate>(this, new PlaneUpdateEvent { UpdatedPlanes = testList });
-        //        logger.Received(1).LogText($"Plane Position, x: {p1.XPosition} , y:{p1.YPosition}");
-        //    }
-
-        //    [TestCase(300.276)]
-        //    public void TestUpdatedPlaneDisplayHeading(double _heading)
-        //    {
-        //        List<Plane> testList = new List<Plane>();
-
-        //        Plane p1 = new Plane { Heading = _heading};
-
-        //        testList.Add(p1);
-        //        pm.PlaneNotify += Raise.Event<PlaneUpdate>(this, new PlaneUpdateEvent { UpdatedPlanes = testList });
-        //        logger.Received(1).LogText($"Plane Heading: {p1.Heading}");
-        //    }
-
-        //    [TestCase(300.276)]
-        //    public void TestUpdatedPlaneDisplayHorizontalSpeed(double _hSpeed)
-        //    {
-        //        List<Plane> testList = new List<Plane>();
-
-        //        Plane p1 = new Plane { HorizontalSpeed = _hSpeed};
-
-        //        testList.Add(p1);
-        //        pm.PlaneNotify += Raise.Event<PlaneUpdate>(this, new PlaneUpdateEvent { UpdatedPlanes = testList });
-        //        logger.Received(1).LogText($"Plane Horizontal speed: {p1.HorizontalSpeed}");
-        //    }
     }
 }
